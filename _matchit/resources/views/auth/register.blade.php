3b25e9bd69715ae5f8db8f903fbb0b57520ff528
Nilaksha Perera <nilakshap@99x.io>
2021-03-05 19:45:03 +0530

    UI improvements

--- _matchit/resources/views/auth/register.blade.php
+++ _matchit/resources/views/auth/register.blade.php
@@ -6,16 +6,17 @@
     <div class="container d-flex align-items-center justify-content-center"
         style="background-image: url('{{ url('/images/auth-background.png') }}')">
         <div class="row inner-container justify-content-center">
-            <div class="col-md-7 tagline-inverted">
-                <div class="row d-flex align-items-end image-holder">
-             
-                </div>
+            <div class="col-md-7 p-4 auth-form-container">
+            
+                    <h2 class="mb-2 "><b>{{ __('Instructions') }}</b></h2>
+                    <b>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</b>
+            
             </div>
             <div class="col-md-5 auth-form-container">
               
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-                        <h2 class="mb-2 pl-3 pr-3"><b>{{ __('Create New Account') }}</b></h2>
+                        <h2 class="mb-2 pl-3 pr-3 text-center"><b>{{ __('Create New Account') }}</b></h2>
 
                         <div class="form-group ">
                             <div class="col-md-12 mb-1-on-mobile">
--- _matchit/resources/views/layouts/app.blade.php
+++ _matchit/resources/views/layouts/app.blade.php
@@ -38,6 +38,9 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link {{ (Route::currentRouteName() == 'about')? 'active' : '' }}" href="{{ url('/about') }}">About</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
